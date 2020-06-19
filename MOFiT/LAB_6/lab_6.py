dc3518bff07a1e0229e3ab2880d12c9e3b36d2ab
Pawel <nigrumsartagine@gmail.com>
2020-06-19 11:09:34 +0200

    Report added

--- MOFiT/LAB_6/lab_6.py
+++ MOFiT/LAB_6/lab_6.py
@@ -21,17 +21,17 @@ u0 = 1
 class siatka:
 	def __init__(self, iteracje = 10**3, nazwa = '', dx = dx, dy = dy, u0 = u0):
 		self.__iteracje = iteracje
-		self.__siatka = (201, 101)
+		self.__siatka = (201, 102)
 		self.__dx = dx
 		self.__dy = dy
 		self.__u0 = u0
 		self.__x = numpy.arange(1, self.__siatka[0] + 1, self.__dx)
 
-		self.__y = numpy.arange(51, self.__siatka[1] + 51, self.__dy)
+		self.__y = numpy.arange(50, self.__siatka[1] + 50, self.__dy)
 
 		self.__fun_strumienia = numpy.zeros(self.__siatka)
 		self.__potencjal =  numpy.zeros(self.__siatka)
-		#self.__potencjal.fill(100)	#Aby szybciej się liczyło
+
 
 		self.__warunki_brzegowe_strumien()
 		self.__warunki_brzegowe_potencjal()
@@ -59,7 +59,7 @@ class siatka:
 
 				#Dół
 
-				if y == 51 and 1 < x < 201:
+				if y == 50 and 1 < x < 201:
 					self.__fun_strumienia[i][j] = self.__fun_strumienia[0][0]
 
 				if 30 < y < 70 and 95 < x < 105:
@@ -70,11 +70,13 @@ class siatka:
 
 	def __warunki_brzegowe_potencjal(self):
 		
+		#Wypełniamy tak jakby przeszkody nie było
 		for i in range(self.__siatka[0]):
 			for j in range(self.__siatka[1]):
 				x = self.__x[i]
 				self.__potencjal[i][j] = self.__u0 * x
 
+		#Narzucamy warunki brzegowe
 		for i in range(self.__siatka[0]):
 			for j in range(self.__siatka[1]):
 
@@ -95,7 +97,7 @@ class siatka:
 
 				#Dół
 
-				if y == 51 and (1 < x < 95 or 105 < x):
+				if y == 50 and (1 < x < 95 or 105 < x):
 					self.__potencjal[i][50-50] = self.__potencjal[i][51-50]
 
 				if (x == 95 or x == 105) and 50 < y <= 70:
@@ -118,7 +120,7 @@ class siatka:
 				x = self.__x[i]
 				y = self.__y[j]
 
-				if y == 51 and (1 < x < 95 or 105 < x):
+				if y == 50 and (1 < x < 95 or 105 < x):
 					self.__potencjal[i][50-50] = self.__potencjal[i][51-50]
 
 				if (x == 95 or x == 105) and 50 < y <= 70:
@@ -191,24 +193,24 @@ class siatka:
 
 		C = ax.contour(X, Y, V, 25, cmap = 'viridis')
 		ax.clabel(C, inline=1, fontsize=10)
-		ax.fill_between([95,105],51,70, color = 'gray')
+		ax.fill_between([95,105],50,70, color = 'gray')
 		ax.grid()
 		ax.set(ylabel = 'y', xlabel = 'x', title = '$\psi(x,y)$')
 		ax.xaxis.label.set_size(18)
 		ax.yaxis.label.set_size(18)
 		ax.title.set_size(18)
-		ax.get_figure().set_size_inches(18.5, 10.5, forward = False)
+		ax.get_figure().set_size_inches(18.5, 10.5, forward = True)
 
 		fig_f, ax_f = plt.subplots()
-		CS = ax_f.contourf(X, Y, V, 25, cmap = 'viridis')
-		ax_f.fill_between([95,105],51,70, color = 'gray')
+		CS = ax_f.contourf(X, Y, V, 50, cmap = 'viridis')
+		ax_f.fill_between([95,105],50,70, color = 'gray')
 		cb_f = fig_f.colorbar(CS)
 		ax_f.grid()
 		ax_f.set(ylabel = 'y', xlabel = 'x', title = '$\psi(x,y)$')
 		ax_f.xaxis.label.set_size(18)
 		ax_f.yaxis.label.set_size(18)
 		ax_f.title.set_size(18)
-		ax_f.get_figure().set_size_inches(18.5, 10.5, forward = False)
+		ax_f.get_figure().set_size_inches(18.5, 10.5, forward = True)
 
 
 		plt.show()
@@ -221,9 +223,10 @@ class siatka:
 		fig, ax = plt.subplots()
 		V = numpy.rot90(self.__potencjal)[::-1]
 
-		C = ax.contour(X, Y, V, levels = 9, cmap = 'viridis')
+		z = [i for i in range(0,201,5)]
+		C = ax.contour(X, Y, V, levels = z, cmap = 'viridis')
 		ax.clabel(C, inline=1, fontsize=10)
-		ax.fill_between([95,105],51,70, color = 'gray')
+		ax.fill_between([95,105],50,70, color = 'gray')
 
 
 		ax.grid()
@@ -231,18 +234,18 @@ class siatka:
 		ax.xaxis.label.set_size(18)
 		ax.yaxis.label.set_size(18)
 		ax.title.set_size(18)
-		ax.get_figure().set_size_inches(18.5, 10.5, forward = False)
+		ax.get_figure().set_size_inches(18.5, 10.5, forward = True)
 
 		fig_f, ax_f = plt.subplots()
-		CS = ax_f.contourf(X, Y, V, levels = 9, cmap = 'viridis')
-		ax_f.fill_between([95,105],51,70, color = 'gray')
+		CS = ax_f.contourf(X, Y, V, levels = 50, cmap = 'viridis')
+		ax_f.fill_between([95,105],50,70, color = 'gray')
 		cb_f = fig_f.colorbar(CS)
 		ax_f.grid()
 		ax_f.set(ylabel = 'y', xlabel = 'x', title = '$\phi(x,y)$')
 		ax_f.xaxis.label.set_size(18)
 		ax_f.yaxis.label.set_size(18)
 		ax_f.title.set_size(18)
-		ax_f.get_figure().set_size_inches(18.5, 10.5, forward = False)
+		ax_f.get_figure().set_size_inches(18.5, 10.5, forward = True)
 
 
 		plt.show()
@@ -267,8 +270,8 @@ def fun_2(N):
 	print((time.time()-t)/60)
 	A.rysuj_potencjal('potencjal')
 
-#fun_1(10**4)
-fun_2(2*10**4)
+fun_1(10**4)
+#fun_2(2*10**4)
 
 
 
